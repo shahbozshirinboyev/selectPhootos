3c6c778d4b26785ce46d4303f7fa315eff5762a5
shahbozshirinboyev <shahboz.sh.b@gmail.com>
2024-12-19 09:10:03 +0500

    Add remove function

--- src/App.jsx
+++ src/App.jsx
@@ -7,14 +7,24 @@ function App() {
     });
 
     const handlePhotos = (e) => {
-
-      console.log(e)
-
       const files = Array.from(e.target.files); // Tanlangan fayllarni massivga o'giramiz
       const urls = files.map((file) => URL.createObjectURL(file)); // Fayllar URLini yaratamiz
       setPhotos({ files, urls }); // Holatni yangilaymiz
       console.log("Tanlangan fayllar:", files); // Fayllarni konsolga chiqaramiz
+    };
 
+    const removePhoto = (index) => {
+      setPhotos((prevState) => {
+        const updatedUrls = [...prevState.urls];
+        const updatedFiles = [...prevState.files];
+        // Massivdan elementni o'chiramiz
+        updatedUrls.splice(index, 1);
+        updatedFiles.splice(index, 1);
+        return {
+          urls: updatedUrls,
+          files: updatedFiles
+        };
+      });
     };
 
     console.log(photos)
@@ -27,14 +37,17 @@ function App() {
         
         <div className="p-12">
 
-        <label htmlFor="selectPhotos" className="flex flex-col border border-dashed rounded-xl justify-center items-center p-6 cursor-pointer select-none">
+        <label htmlFor="selectPhotos" className="z-[5] flex flex-col border border-dashed rounded-xl justify-center items-center p-6 cursor-pointer select-none">
           
           <span className={`font-medium ${photos.urls.length === 0 ? "" : "hidden"}`}>Добавьте фото</span>
           <span className={`${photos.urls.length === 0 ? "" : "hidden"}`}>Перетащите сюда или <span className="text-sky-600">выберите на компьютере</span></span>
 
-          <div>
-            { photos.urls.map((photo, index) => (
-            <img key={index} src={photo} className="w-[90px] h-[90px]" />
+          <div className="flex justify-start items-center gap-4">
+            {photos.urls.map((photo, index) => (
+              <div key={index} className="relative">
+                <span onClick={()=> { removePhoto(index) }} className="absolute z-[25] -right-2 -top-2 bg-white hover:bg-gray-200 w-[30px] h-[30px] border rounded-full p-1 flex justify-center items-center text-[12px]">❌</span>
+                <img  src={photo} className="w-[90px] h-[90px] border z-[10]" />
+              </div>
             ))}
           </div>
 
