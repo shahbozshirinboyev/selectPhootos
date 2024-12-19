de8502cd339741353d6572f757a495b002ae5a03
shahbozshirinboyev <shahboz.sh.b@gmail.com>
2024-12-19 10:15:42 +0500

    Fix some bugs

--- src/App.jsx
+++ src/App.jsx
@@ -7,17 +7,15 @@ function App() {
   });
 
   const handlePhotos = (e) => {
-    const files = Array.from(e.target.files); // Tanlangan fayllarni massivga o'giramiz
-    const urls = files.map((file) => URL.createObjectURL(file)); // Fayllar URLini yaratamiz
-    setPhotos({ files, urls }); // Holatni yangilaymiz
-    console.log("Tanlangan fayllar:", files); // Fayllarni konsolga chiqaramiz
+    const files = Array.from(e.target.files);
+    const urls = files.map((file) => URL.createObjectURL(file));
+    setPhotos({ files, urls });
   };
 
   const removePhoto = (index) => {
     setPhotos((prevState) => {
       const updatedUrls = [...prevState.urls];
       const updatedFiles = [...prevState.files];
-      // Massivdan elementni o'chiramiz
       updatedUrls.splice(index, 1);
       updatedFiles.splice(index, 1);
       return {
@@ -37,23 +35,21 @@ function App() {
 
         <div className="p-12">
 
-          <label htmlFor="selectPhotos" className="z-[5] flex flex-col border border-dashed rounded-xl justify-start p-4 cursor-pointer select-none">
+          <label htmlFor="selectPhotos" className="flex flex-col border border-dashed rounded-xl justify-start p-4 cursor-pointer select-none">
             <div className="flex flex-col items-center">
-              <span className={`font-medium ${photos.urls.length === 0 ? "" : "hidden"}`}>Добавьте фото</span>
-              <span className={`${photos.urls.length === 0 ? "" : "hidden"}`}>Перетащите сюда или <span className="text-sky-600">выберите на компьютере</span></span>
+              <span className={`text-[14px] md:text-[16px] font-medium ${photos.urls.length === 0 ? "" : "hidden"}`}>Добавьте фото</span>
+              <span className={`${photos.urls.length === 0 ? "" : "hidden"} text-[12px] md:text-[14px]`}>Перетащите сюда или <span className="text-sky-600">выберите на компьютере</span></span>
             </div>
-
-            <div className="flex justify-start items-center gap-4" >
+            <div className="flex flex-wrap justify-start items-center gap-4" >
               {photos.urls.map((photo, index) => (
                 <div key={index} className="relative">
-                  <span onClick={(e) => { e.preventDefault(); removePhoto(index); }} className="absolute z-[25] -right-2 -top-2 bg-white hover:bg-gray-200 w-[30px] h-[30px] border rounded-full p-1 flex justify-center items-center text-[12px]">❌</span>
-                  <img src={photo} className="w-[75px] h-[75px] border z-[10]" />
+                  <span onClick={(e) => { e.preventDefault(); removePhoto(index); }} className="absolute -right-2 -top-2 bg-white hover:bg-red-100 w-[25px] h-[25px] border border-red-100 rounded-full p-1 flex justify-center items-center text-[10px]">❌</span>
+                  <img src={photo} className="w-[70px] h-[70px] border border-red-100" />
                 </div>
               ))}
             </div>
-
+            <input id="selectPhotos" className="hidden" type="file" name="photos" multiple onChange={handlePhotos} />
           </label>
-          <input id="selectPhotos" className="hidden" type="file" name="photos" multiple onChange={handlePhotos} />
 
         </div>
 
